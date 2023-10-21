99289d911843777c3da09e195fcb77ab8fc2274d
Sanskar <sanskarshaurya0@gmail.com>
2023-10-21 21:23:18 +0530

    Used the map

--- phase1/trader1.cpp
+++ phase1/trader1.cpp
@@ -1,35 +1,34 @@
 #include "receiver.h"
-#include "map.h"
 #include <vector>
 #include <string>
 
-// class Stock
-// {
-// public:
-//     std::string name;
-//     int value;
-//     Stock(const std::string &stockName, int stockValue) : name(stockName), value(stockValue) {}
+class Stock
+{
+public:
+    std::string name;
+    int value;
+    Stock(const std::string &stockName, int stockValue) : name(stockName), value(stockValue) {}
 
-//     // Overload the ">" operator to compare based on name
-//     bool operator>(const Stock &other) const
-//     {
-//         return std::strcmp(name.c_str(), other.name.c_str()) > 0;
-//     }
-//     bool operator<(const Stock& other) const {
-//         return std::strcmp(name.c_str(), other.name.c_str()) < 0;
-//     }
-// };
+    // Overload the ">" operator to compare based on name
+    bool operator>(const Stock &other) const
+    {
+        return std::strcmp(name.c_str(), other.name.c_str()) > 0;
+    }
+    bool operator<(const Stock& other) const {
+        return std::strcmp(name.c_str(), other.name.c_str()) < 0;
+    }
+};
 
-// void add_stock(std::vector<Stock*>& v, Stock* s){
-//     v.push_back(s);
-// }
+void add_stock(std::vector<Stock*>& v, Stock* s){
+    v.push_back(s);
+}
 
-// Stock* search_stock(std::vector<Stock*>& v, std::string s){
-//     for(auto a: v){
-//         if(a->name == s) return a;
-//     }
-//     return NULL;
-// }
+Stock* search_stock(std::vector<Stock*>& v, std::string s){
+    for(auto a: v){
+        if(a->name == s) return a;
+    }
+    return NULL;
+}
 
 int main() {
 
@@ -42,9 +41,9 @@ int main() {
         input.push_back(message.substr(start, end - start));
         start = end + 2;
     }
+
     delimiter = ' ';
-    // std::vector<Stock*> stocks;
-    Map stocks;
+    std::vector<Stock*> stocks;
     for (auto i : input){
         std::vector<std::string> order;
         order.resize(3);
@@ -57,26 +56,28 @@ int main() {
             start = end + 1;
         }
         order[k] = i.substr(start);
-
-        mystring curr = mystring(order[0]);
-        if (stocks.find(curr) == 0){
-            stocks[curr] = stoi(order[1]);
+        Stock* curr = search_stock(stocks, order[0]);
+        if (curr == NULL){
+            Stock* newStock = new Stock (order[0],stoi(order[1]));
+            stocks.push_back(newStock);
             std::cout << order[0] << " " << order[1] << " ";
             if (order[2]=="s") std::cout << "b\n";
             else std::cout << "s\n";
         }else{
             if(order[2]=="s"){
-                if(stoi(order[1]) < stocks[curr]){
+                if(stoi(order[1]) < curr->value){
                     std::cout << order[0] << " " << order[1] << " " << "b\n";
-                    stocks[curr] = stoi(order[1]);
+                    curr->value = stoi(order[1]);
                 } else std::cout << "No trade\n";
             } else {
-                if(stoi(order[1]) > stocks[curr]){
+                if(stoi(order[1]) > curr->value){
                     std::cout << order[0] << " " << order[1] << " " << "s\n";
-                    stocks[curr] = stoi(order[1]);
+                    curr->value = stoi(order[1]);
                 } else std::cout << "No trade\n";
             }
         }
     }
+
+
     return 0;
 }
