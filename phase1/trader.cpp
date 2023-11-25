51a1c32c2ddbc964f248fc80a2fd0a33eb5adc98
arihantv140305 <arihant.vashista14@gmail.com>
2023-11-25 19:14:04 +0530

    finished phase1 for good

--- phase1/trader.cpp
+++ phase1/trader.cpp
@@ -1,13 +1,91 @@
 #include "receiver.h"
-#include "map.h"
 #include <vector>
 #include <string>
 #include "Stock.h"
+#include "Package.h"
 
-
-int main()
+void get_max_sum(vector<Package> &packages)
 {
+    RedBlackTree<myPair<int>> maxSum;
+    for (auto i : packages)
+    {
+        for (auto j : i.stocks)
+        {
+            if (j.second * i.isBuy > 0)
+            {
+                maxSum.search(j.first).first += j.second * i.isBuy * i.q2;
+            }
+            else
+            {
+                maxSum.search(j.first).second += j.second * i.isBuy * i.q2;
+            }
+        }
+    }
+    for (auto it = packages.begin(); it != packages.end(); it++)
+        it->set_max(maxSum);
+    bool flag = true;
+    for (auto it = packages.begin(); it != packages.end(); it++)
+    {
+        if (it->q2 != it->maxQuantity)
+        {
+            it->q2 = it->maxQuantity;
+            flag = false;
+        }
+    }
+    if (flag)
+    {
+        for (auto it = packages.begin(); it != packages.end(); it++)
+            it->q2 = it->quantity;
+        return;
+    }
+    if (!flag)
+        get_max_sum(packages);
+}
 
+int find_all_c(std::vector<Package> &packages, std::vector<int> &results)
+{
+    int n = packages.size();
+    int profit = 0;
+    std::vector<int> c(n, 0);
+    std::vector<int> ans(n, 0);
+    c[n - 1] = 0;
+    while (true)
+    {
+        Package sum;
+        for (int i = 0; i < n; ++i)
+        {
+            Package temp = packages[i] * c[i];
+            sum = sum + temp;
+            // sum.printPackage();
+            // std::cout <<std::endl << "-----" << std::endl;
+        }
+
+        if (sum.isArbitrage() && sum.price > profit)
+        {
+            ans = c;
+            profit = sum.price;
+        }
+
+        // Increment the rightmost index
+        int i = n - 1;
+        while (i >= 0 && ++c[i] > packages[i].maxQuantity)
+        {
+            c[i--] = 0;
+        }
+
+        // Check if we've finished iterating
+        if (i < 0)
+        {
+            break;
+        }
+    }
+    results = ans;
+    return profit;
+}
+
+int main(int argc, char *argv[])
+{
+    char choice = argv[1][0];
     Receiver rcv;
     std::string message = "";
     while (true)
@@ -25,141 +103,321 @@ int main()
         start = end + 2;
     }
     delimiter = ' ';
-    RedBlackTree<Stock> stocks;
-    int check;
-    for (auto i : input)
+    if (choice == '1')
     {
-        std::vector<std::string> order;
-        order.resize(3);
-        start = 0;
-        end = 0;
-        int k = 0;
-        while ((end = i.find(delimiter, start)) != std::string::npos)
+        RedBlackTree<Stock> stocks;
+        int check;
+        for (auto i : input)
         {
-            order[k] = i.substr(start, end - start);
-            k++;
-            start = end + 1;
-        }
-        order[k] = i.substr(start);
-
-        mystring curr = mystring(order[0]);
-        if (stocks.find(curr) == 0)
-        {
-            stocks.insert(curr, Stock(stoi(order[1])));
-            stocks.search(curr).standard = stoi(order[1]);
-            std::cout << order[0] << " " << order[1] << " ";
-            if (order[2] == "s")
-                std::cout << "b\r\n";
-            else
-                std::cout << "s\r\n";
-        }
-        else
-        {
-            if (order[2] == "s")
+            std::vector<std::string> order;
+            order.resize(3);
+            start = 0;
+            end = 0;
+            int k = 0;
+            while ((end = i.find(delimiter, start)) != std::string::npos)
             {
-                if(stocks.search(curr).hasBestBuying)
+                order[k] = i.substr(start, end - start);
+                k++;
+                start = end + 1;
+            }
+            order[k] = i.substr(start);
+
+            mystring curr = mystring(order[0]);
+            if (stocks.find(curr) == 0)
+            {
+                stocks.insert(curr, Stock(stoi(order[1])));
+                stocks.search(curr).standard = stoi(order[1]);
+                std::cout << order[0] << " " << order[1] << " ";
+                if (order[2] == "s")
+                    std::cout << "b\r\n";
+                else
+                    std::cout << "s\r\n";
+            }
+            else
+            {
+                if (order[2] == "s")
                 {
-                    if(stoi(order[1]) >= stocks.search(curr).bestBuying)
+                    if (stocks.search(curr).hasBestBuying)
                     {
-                        std::cout << "No trade" << "\r\n";
-                        continue;
+                        if (stoi(order[1]) >= stocks.search(curr).bestBuying)
+                        {
+                            std::cout << "No trade"
+                                      << "\r\n";
+                            continue;
+                        }
                     }
-                }
-                if (stocks.search(curr).hasBestSelling)
-                {
-                    if (stoi(order[1]) == stocks.search(curr).bestSelling)
+                    if (stocks.search(curr).hasBestSelling)
                     {
-                        std::cout << "No trade"
-                                  << "\r\n";
-                        stocks.search(curr).hasBestSelling = 0;
+                        if (stoi(order[1]) == stocks.search(curr).bestSelling)
+                        {
+                            std::cout << "No trade"
+                                      << "\r\n";
+                            stocks.search(curr).hasBestSelling = 0;
+                        }
+                        else
+                        {
+                            goto banyan;
+                        }
                     }
                     else
                     {
-                        goto banyan;
+                    banyan:
+                        if (stocks.search(curr).standard > stoi(order[1]))
+                        {
+                            std::cout << order[0] << " " << order[1] << " "
+                                      << "b\r\n";
+                            stocks.search(curr).standard = stoi(order[1]);
+                            stocks.search(curr).hasBestBuying = 0;
+                        }
+                        else
+                        {
+                            std::cout << "No trade"
+                                      << "\r\n";
+                            if (stocks.search(curr).hasBestBuying)
+                            {
+                                if (stocks.search(curr).bestBuying > stoi(order[1]))
+                                {
+                                    stocks.search(curr).bestBuying = stoi(order[1]);
+                                }
+                            }
+                            else
+                            {
+                                stocks.search(curr).bestBuying = stoi(order[1]);
+                                stocks.search(curr).hasBestBuying = 1;
+                            }
+                        }
                     }
                 }
                 else
                 {
-                banyan:
-                    if (stocks.search(curr).standard > stoi(order[1]))
+                    if (stocks.search(curr).hasBestSelling)
                     {
-                        std::cout << order[0] << " " << order[1] << " "
-                                  << "b\r\n";
-                        stocks.search(curr).standard = stoi(order[1]);
-                        stocks.search(curr).hasBestBuying = 0;
-                    }
-                    else
-                    {
-                        std::cout << "No trade"
-                                  << "\r\n";
-                        if (stocks.search(curr).hasBestBuying)
+                        if (stoi(order[1]) <= stocks.search(curr).bestSelling)
                         {
-                            if (stocks.search(curr).bestBuying > stoi(order[1]))
-                            {
-                                stocks.search(curr).bestBuying = stoi(order[1]);
-                            }
+                            std::cout << "No trade"
+                                      << "\r\n";
+                            continue;
+                        }
+                    }
+                    if (stocks.search(curr).hasBestBuying)
+                    {
+                        if (stoi(order[1]) == stocks.search(curr).bestBuying)
+                        {
+                            std::cout << "No trade"
+                                      << "\r\n";
+                            stocks.search(curr).hasBestBuying = 0;
                         }
                         else
                         {
-                            stocks.search(curr).bestBuying = stoi(order[1]);
-                            stocks.search(curr).hasBestBuying = 1;
+                            goto chaddi;
+                        }
+                    }
+                    else
+                    {
+                    chaddi:
+                        if (stocks.search(curr).standard < stoi(order[1]))
+                        {
+                            std::cout << order[0] << " " << order[1] << " "
+                                      << "s\r\n";
+                            stocks.search(curr).standard = stoi(order[1]);
+                            stocks.search(curr).hasBestSelling = 0;
+                        }
+                        else
+                        {
+                            std::cout << "No trade"
+                                      << "\r\n";
+                            if (stocks.search(curr).hasBestSelling)
+                            {
+                                if (stocks.search(curr).bestSelling < stoi(order[1]))
+                                {
+                                    stocks.search(curr).bestSelling = stoi(order[1]);
+                                }
+                            }
+                            else
+                            {
+                                stocks.search(curr).bestSelling = stoi(order[1]);
+                                stocks.search(curr).hasBestSelling = 1;
+                            }
                         }
                     }
                 }
             }
-            else
+        }
+    }
+    else if (choice == '2')
+    {
+        vector<Package> packages;
+        int net_profit = 0;
+        for (auto ms : input)
+        {
+            Package P1(ms);
+            bool flag = 0;
+            for (auto it = packages.begin(); it != packages.end(); it++)
             {
-                if(stocks.search(curr).hasBestSelling)
+                auto i = *it;
+                if (i == P1)
                 {
-                    if(stoi(order[1]) <= stocks.search(curr).bestSelling)
+                    if (i.isBuy == P1.isBuy)
                     {
-                        std::cout << "No trade" << "\r\n";
-                        continue;
-                    }
-                }
-                if (stocks.search(curr).hasBestBuying)
-                {
-                    if (stoi(order[1]) == stocks.search(curr).bestBuying)
-                    {
-                        std::cout << "No trade"
-                                  << "\r\n";
-                        stocks.search(curr).hasBestBuying = 0;
-                    }
-                    else
-                    {
-                        goto chaddi;
-                    }
-                }
-                else
-                {
-                chaddi:
-                    if (stocks.search(curr).standard < stoi(order[1]))
-                    {
-                        std::cout << order[0] << " " << order[1] << " "
-                                  << "s\r\n";
-                        stocks.search(curr).standard = stoi(order[1]);
-                        stocks.search(curr).hasBestSelling = 0;
-                    }
-                    else
-                    {
-                        std::cout << "No trade"
-                                  << "\r\n";
-                        if (stocks.search(curr).hasBestSelling)
+                        if (P1.price * P1.isBuy <= i.price * i.isBuy)
                         {
-                            if (stocks.search(curr).bestSelling < stoi(order[1]))
-                            {
-                                stocks.search(curr).bestSelling = stoi(order[1]);
-                            }
+                            flag = 1;
+                            break;
                         }
                         else
                         {
-                            stocks.search(curr).bestSelling = stoi(order[1]);
-                            stocks.search(curr).hasBestSelling = 1;
+                            packages.erase(it);
+                            break;
+                        }
+                    }
+                    else if (i.isBuy + P1.isBuy == 0)
+                    {
+                        if (i.price == P1.price)
+                        {
+                            flag = 1;
+                            packages.erase(it);
+                            break;
                         }
                     }
                 }
-            }   
+            }
+            if (flag)
+            {
+                std::cout << "No Trade" << std::endl;
+                continue;
+            }
+            Package bestPackage;
+            int maxProfit = 0;
+            int n = packages.size();
+            for (int i = 0; i < (1 << n); i++)
+            {
+                Package P = P1;
+                P.indexes.push_back(n);
+                for (int j = n - 1; j >= 0; j--)
+                {
+                    if ((i & (1 << j)) != 0)
+                    {
+                        P = P + packages[j];
+                        P.indexes.push_back(j);
+                    }
+                }
+                int check = P.isArbitrage();
+                if (P.price > maxProfit && check)
+                {
+                    maxProfit = P.price;
+                    bestPackage = P;
+                }
+            }
+            packages.push_back(P1);
+            if (bestPackage.indexes.size() == 0)
+                std::cout << "No Trade" << std::endl;
+            else
+            {
+                for (auto k : bestPackage.indexes)
+                {
+                    packages[k].printPackage();
+                    packages.erase(packages.begin() + k);
+                }
+                net_profit += bestPackage.price;
+            }
         }
+        std::cout << net_profit << std::endl;
+    }
+    else if (choice == '3')
+    {
+        vector<Package> packages;
+        int net_profit = 0;
+        bool skip = false;
+        for (auto ms : input)
+        {
+            Package P1(ms);
+            bool flag = 0;
+            // std::cout << P1.quantity << std::endl;
+            for (auto it = packages.begin(); it != packages.end(); it++)
+            {
+                auto i = *it;
+                if (i == P1 && i.price == P1.price)
+                {
+                    if (P1.isBuy == i.isBuy)
+                    {
+                        it->quantity += P1.quantity;
+                        it->maxQuantity += P1.quantity;
+                    }
+                    else
+                    {
+                        if (i.quantity > P1.quantity)
+                        {
+                            it->quantity -= P1.quantity;
+                            skip = true;
+                        }
+                        else
+                        {
+                            P1.quantity = P1.quantity - i.quantity;
+                            packages.erase(it);
+                            if (P1.quantity != 0)
+                            {
+                                flag = 0;
+                                break;
+                            }
+                            else
+                            {
+                                skip = true;
+                            }
+                        }
+                    }
+                    flag = 1;
+                    break;
+                }
+            }
+
+            if (skip)
+            {
+                skip = false;
+                std::cout<<"No Trade"<<std::endl;
+                get_max_sum(packages);
+                continue;
+            }
+
+            if (!flag)
+            {
+                packages.push_back(P1);
+            }
+
+            vector<int> results;
+            get_max_sum(packages);
+
+            // for(auto i : packages){
+            //     std::cout << i.maxQuantity << " " << std::endl;
+            // }
+
+            bool hasArbitrage = false;
+
+            bool flag2 = false;
+            net_profit += find_all_c(packages, results);
+            int ind = results.size() - 1;
+            for (int i = packages.size() - 1; i >= 0; i--)
+            {
+                if (results[ind] != 0)
+                {
+                    packages[i].printPackage();
+                    char b;
+                    b = (packages[i].isBuy == 1) ? 's' : 'b';
+                    std::cout << results[ind] << " " << b << std::endl;
+                    packages[i].quantity -= results[ind];
+                    packages[i].maxQuantity -= results[ind];
+                    flag2 = true;
+                    hasArbitrage = true;
+                }
+                ind--;
+            }
+            get_max_sum(packages);
+
+            if (!hasArbitrage)
+            {
+                std::cout << "No Trade" << std::endl;
+            }
+        }
+        std::cout << net_profit << std::endl;
     }
     return 0;
 }
--- phase1/trader2.cpp
+++ phase1/trader2.cpp
@@ -31,7 +31,6 @@ int main()
         bool flag = 0;
         for(auto it = packages.begin(); it != packages.end(); it++){
             auto i = *it;
-            // std::cout<<i.stocks.nodeCount<<P1.stocks.nodeCount<<std::endl;
             if(i == P1){
                 if(i.isBuy == P1.isBuy){
                     if(P1.price*P1.isBuy <= i.price*i.isBuy){
@@ -44,15 +43,6 @@ int main()
                         packages.erase(it);
                         break;
                     }
-                    // else if(i.price > P1.price){
-                    //     flag = 1;
-                    //     it->price = i.price - P1.price;
-                    //     break;
-                    // }else{
-                    //     P1.price = P1.price - i.price;
-                    //     packages.erase(it);
-                    //     break;
-                    // }
                 }
             }
         }
@@ -64,26 +54,19 @@ int main()
         {
             Package P = P1;
             P.indexes.push_back(n);
-            // std::cout<<n<<" ";
             for (int j = n - 1; j >= 0; j--)
             {
                 if ((i & (1 << j)) != 0){
                     P = P + packages[j];
                     P.indexes.push_back(j);
-                    // std::cout<<j<<" ";
                 }
             }
-            // std::cout<<std::endl;
             int check = P.isArbitrage();
-            // P.printPackage();
-            // if(check) std::cout<<"Detected Arbitrage " << P.price<<std::endl;
             if(P.price > maxProfit && check){
                 maxProfit = P.price;
                 bestPackage = P;
             }
-            // std::cout<<"----"<<std::endl;
         }
-        // std::cout<<P1.stocks.nodeCount<<std::endl;
         packages.push_back(P1); 
         if(bestPackage.indexes.size() == 0)std::cout<<"No Trade"<<std::endl;
         else{
@@ -93,7 +76,6 @@ int main()
             }
             net_profit+=bestPackage.price;
         }
-        // std::cout<<"----------------"<<std::endl;
     }
     std::cout << net_profit << std::endl;
     for(auto i : packages){
