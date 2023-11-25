9b1a9631d19f22c2d58f703f6854a12c1d84c81b
arihantv140305 <arihant.vashista14@gmail.com>
2023-11-25 16:02:28 +0530

    madde some changes

--- phase1/Package.h
+++ phase1/Package.h
@@ -2,7 +2,6 @@
 #include <string>
 #include "map.h"
 
-
 class Package
 {
 public:
@@ -10,10 +9,12 @@ public:
     int price = 0;
     int isBuy;
     int quantity;
+    int q2;
     int maxQuantity;
     vector<int> indexes;
-    Package() {};
-    Package(std::string i) {
+    Package(){};
+    Package(std::string i)
+    {
         char delimiter = ' ';
         std::vector<std::string> order;
         size_t start = 0;
@@ -24,65 +25,121 @@ public:
             start = end + 1;
         }
         order.push_back(i.substr(start));
-        for(int j = 0; j<order.size()-3; j+=2){
-            stocks.insert(order[j],stoi(order[j+1]));
+        for (int j = 0; j < order.size() - 3; j += 2)
+        {
+            stocks.insert(order[j], stoi(order[j + 1]));
         }
-        stocks.nodeCount = (order.size() - 2)/2;
-        isBuy = (order[order.size()-1] == "b") ? 1 : -1;
-        if(order.size()%2==0) {
-            price = stoi(order[order.size()-2]);
-            quantity=1;
+        stocks.nodeCount = (order.size() - 2) / 2;
+        isBuy = (order[order.size() - 1] == "b") ? 1 : -1;
+        if (order.size() % 2 == 0)
+        {
+            price = stoi(order[order.size() - 2]);
+            quantity = 1;
         }
-        else {
-            quantity = stoi(order[order.size()-2]);
-            price = stoi(order[order.size()-3]);
+        else
+        {
+            quantity = stoi(order[order.size() - 2]);
+            price = stoi(order[order.size() - 3]);
         }
         maxQuantity = quantity;
+        q2 = quantity;
     }
-    
-    Package(RedBlackTree<int> input, int isBuy, int price, vector<int> indexes, int quantity) : stocks(input), isBuy(isBuy), price(price) ,indexes(indexes), quantity(quantity), maxQuantity(quantity) {};
-    Package(const Package& other)
-    : price(other.price), isBuy(other.isBuy), indexes(other.indexes), quantity(other.quantity),maxQuantity(other.maxQuantity){
-        RedBlackTree<int> temp (other.stocks);
+
+    Package(RedBlackTree<int> input, int isBuy, int price, vector<int> indexes, int quantity) : stocks(input), isBuy(isBuy), price(price), indexes(indexes), quantity(quantity), maxQuantity(quantity), q2(quantity){};
+    Package(const Package &other)
+        : price(other.price), isBuy(other.isBuy), indexes(other.indexes), quantity(other.quantity), maxQuantity(other.maxQuantity), q2(other.q2)
+    {
+        RedBlackTree<int> temp(other.stocks);
         stocks = temp;
     }
-    bool operator==(Package& rhs){
+    bool operator==(Package &rhs)
+    {
         return (this->stocks == rhs.stocks);
     }
 
-    Package operator*(int rhs){
-        RedBlackTree<int> temp = this->stocks;
-        for(auto i: temp) temp.search(i.first) = i.second * rhs;
-        return Package(temp,this->isBuy,this->price*rhs, this->indexes, this->quantity);
-    }
-
-    Package operator+(Package& rhs)
+    Package operator*(int rhs)
     {
-        RedBlackTree<int> temp = this->stocks; 
-        for(auto i: temp) temp.search(i.first) = i.second * this->isBuy;
-        for(auto i : rhs.stocks){
-            temp.search(i.first) = temp.search(i.first) + (i.second)*(rhs.isBuy);
-        }
-        int res = this->price*this->isBuy + rhs.price*rhs.isBuy;
-        return Package(temp,1,res, this->indexes,this->quantity);
+        RedBlackTree<int> temp = this->stocks;
+        for (auto i : temp)
+            temp.search(i.first) = i.second * rhs;
+        return Package(temp, this->isBuy, this->price * rhs, this->indexes, this->quantity);
     }
 
-    bool isArbitrage(){
-        for(auto i : stocks){
-            if(i.second != 0){
+    Package operator+(Package &rhs)
+    {
+        RedBlackTree<int> temp = this->stocks;
+        for (auto i : temp)
+            temp.search(i.first) = i.second * this->isBuy;
+        for (auto i : rhs.stocks)
+        {
+            temp.search(i.first) = temp.search(i.first) + (i.second) * (rhs.isBuy);
+        }
+        int res = this->price * this->isBuy + rhs.price * rhs.isBuy;
+        return Package(temp, 1, res, this->indexes, this->quantity);
+    }
+
+    bool isArbitrage()
+    {
+        for (auto i : stocks)
+        {
+            if (i.second != 0)
+            {
                 return false;
             }
         }
         return true;
     }
-    void printPackage(){
-        for(auto i : stocks){
+    void printPackage()
+    {
+        for (auto i : stocks)
+        {
             std::cout << i.first.name << " " << i.second << " ";
         }
         std::cout << price << " ";
         char b;
         b = (isBuy == 1) ? 's' : 'b';
-        std::cout << quantity<<" max "<<maxQuantity<< " " << b << "#" << std::endl;
+        std::cout << quantity << " max " << maxQuantity << " " << b << "#" << std::endl;
+    }
+
+    void set_max(RedBlackTree<myPair<int>> maxSum)
+    {
+        for (auto k : this->stocks)
+        {
+            auto a = this->maxQuantity;
+            auto b = maxSum.search(k.first);
+            auto c = this->stocks.search(k.first);
+            auto d = (-1 * b.second + 1) / c;
+            auto e = (b.first + 1) / (c * -1);
+            if (c > 0)
+            {
+                if (b.second == 0)
+                {
+                    this->maxQuantity = 0;
+                    break;
+                }
+                else
+                {
+                    if (a == 0)
+                        this->maxQuantity = min(this->q2, d);
+                    else
+                        this->maxQuantity = min(a, d);
+                }
+            }
+            else
+            {
+                if (b.first == 0)
+                {
+                    this->maxQuantity = 0;
+                    break;
+                }
+                else
+                {
+                    if (a == 0)
+                        this->maxQuantity = min(this->q2, e);
+                    else
+                        this->maxQuantity = min(a, e);
+                }
+            }
+        }
     }
 };
-
--- phase1/trader3.cpp
+++ phase1/trader3.cpp
@@ -4,6 +4,44 @@
 #include "Stock.h"
 #include "Package.h"
 
+void get_max_sum(vector<Package> &packages)
+{
+    RedBlackTree<myPair<int>> maxSum;
+    for (auto i : packages)
+    {
+        for (auto j : i.stocks)
+        {
+            if (j.second * i.isBuy > 0)
+            {
+                maxSum.search(j.first).first += j.second * i.isBuy * i.q2;
+            }
+            else
+            {
+                maxSum.search(j.first).second += j.second * i.isBuy * i.q2;
+            }
+        }
+    }
+    for (auto it = packages.begin(); it != packages.end(); it++)
+        it->set_max(maxSum);
+    bool flag = true;
+    for (auto it = packages.begin(); it != packages.end(); it++)
+    {
+        if (it->q2 != it->maxQuantity)
+        {
+            it->q2 = it->maxQuantity;
+            flag = false;
+        }
+    }
+    if (flag)
+    {
+        for (auto it = packages.begin(); it != packages.end(); it++)
+            it->q2 = it->quantity;
+        return;
+    }
+    if (!flag)
+        get_max_sum(packages);
+}
+
 int main()
 {
     Receiver rcv;
@@ -24,53 +62,43 @@ int main()
     }
     delimiter = ' ';
     vector<Package> packages;
-    RedBlackTree<myPair<int>> maxSum; 
+    RedBlackTree<myPair<int>> maxSum;
     int net_profit = 0;
     bool skip = false;
-    for(auto ms : input){
+    for (auto ms : input)
+    {
         Package P1(ms);
         bool flag = 0;
         // std::cout << P1.quantity << std::endl;
-        for(auto it = packages.begin(); it != packages.end(); it++){
+        for (auto it = packages.begin(); it != packages.end(); it++)
+        {
             auto i = *it;
-            if(i == P1 && i.price == P1.price){
-                if(P1.isBuy == i.isBuy){
+            if (i == P1 && i.price == P1.price)
+            {
+                if (P1.isBuy == i.isBuy)
+                {
                     it->quantity += P1.quantity;
-                    for(auto i : P1.stocks){
-                        if(i.second*P1.isBuy > 0){
-                            maxSum.search(i.first).first += i.second*P1.isBuy*P1.quantity;
-                        }else{
-                            maxSum.search(i.first).second += i.second*P1.isBuy*P1.quantity;
-                        }
-                    }
-                }else{
-                    if(i.quantity > P1.quantity){
+                }
+                else
+                {
+                    if (i.quantity > P1.quantity)
+                    {
                         it->quantity -= P1.quantity;
-                        for(auto j : P1.stocks){
-                            if(j.second*i.isBuy > 0){
-                                maxSum.search(j.first).first -= j.second*i.isBuy*P1.quantity;
-                            }else{
-                                maxSum.search(j.first).second -= j.second*i.isBuy*P1.quantity;
-                            }
-                        }
                         skip = true;
-                    }else{
+                    }
+                    else
+                    {
                         P1.quantity = P1.quantity - i.quantity;
-                        for(auto j : i.stocks){
-                            if(j.second*i.isBuy > 0){
-                                maxSum.search(j.first).first -= j.second*i.isBuy*i.quantity;
-                            }else{
-                                maxSum.search(j.first).second -= j.second*i.isBuy*i.quantity;
-                            }
-                        }
                         packages.erase(it);
-                        if(P1.quantity!=0) {
+                        if (P1.quantity != 0)
+                        {
                             flag = 0;
                             break;
-                        }else{
+                        }
+                        else
+                        {
                             skip = true;
                         }
-                        
                     }
                 }
                 flag = 1;
@@ -78,87 +106,22 @@ int main()
             }
         }
 
-        if(skip){
+        if (skip)
+        {
             skip = false;
             continue;
         }
 
-        if(!flag) {
+        if (!flag)
+        {
             packages.push_back(P1);
-            for(auto i : P1.stocks){
-                if(i.second*P1.isBuy > 0){
-                    maxSum.search(i.first).first += i.second*P1.isBuy*P1.quantity;
-                }else{
-                    maxSum.search(i.first).second += i.second*P1.isBuy*P1.quantity;
-                }
-            }
-        
-            // for(auto i: packages){
-            //     std::cout << i.maxQuantity << std::endl;
-            // }
-            for(auto it = packages.begin(); it != packages.end(); it++){
-                for(auto j : P1.stocks){
-                    if(it->stocks.find(j.first)){
-                        // std::cout << it->maxQuantity << std::endl;
-                        if(it->stocks.search(j.first) > 0){
-                            it->maxQuantity = min(it->maxQuantity, -1*(maxSum.search(j.first).second + 1)/it->stocks.search(j.first));
-                        }else{
-                            it->maxQuantity = min(it->maxQuantity, -1*(maxSum.search(j.first).first + 1)/it->stocks.search(j.first));
-                        }
-                    }   
-                }
-            }
         }
+        get_max_sum(packages);
         std::cout << "No Trade" << std::endl;
-        
-
-
-
-        // Package bestPackage;
-        // int maxProfit = 0;
-        // int n = packages.size();
-        // for (int i = 0; i < (1 << n); i++)
-        // {
-        //     Package P = P1;
-        //     P.indexes.push_back(n);
-        //     // std::cout<<n<<" ";
-
-        //     for (int j = n - 1; j >= 0; j--)
-        //     {
-        //         if ((i & (1 << j)) != 0){
-        //             P = P + packages[j];
-        //             P.indexes.push_back(j);
-        //             // std::cout<<j<<" ";
-        //         }
-        //     }
-        //     // std::cout<<std::endl;
-        //     int check = P.isArbitrage();
-        //     // P.printPackage();
-        //     // if(check) std::cout<<"Detected Arbitrage " << P.price<<std::endl;
-        //     if(P.price > maxProfit && check){
-        //         maxProfit = P.price;
-        //         bestPackage = P;
-        //     }
-        //     // std::cout<<"----"<<std::endl;
-        // }
-        // // std::cout<<P1.stocks.nodeCount<<std::endl;
-        // packages.push_back(P1); 
-        // if(bestPackage.indexes.size() == 0)std::cout<<"No Trade"<<std::endl;
-        // else{
-        //     for(auto k : bestPackage.indexes){
-        //         packages[k].printPackage();
-        //         packages.erase(packages.begin() + k);
-        //     }
-        //     net_profit+=bestPackage.price;
-        // }
-        // std::cout<<"----------------"<<std::endl;
     }
-    // std::cout << net_profit << std::endl;
-    for(auto i : packages){
+    for (auto i : packages)
+    {
         i.printPackage();
-    }
-    for(auto i : maxSum){
-        std::cout << i.first.name << " " << i.second.first << " " << i.second.second << std::endl;
     }
     return 0;
 }
